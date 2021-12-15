f6990d5c73486f8fb91b0dc30a27d3f13877a490
Sameer Shah <shahs58@cardiff.ac.uk>
2021-12-15 19:41:33 +0000

    have mastered the art of converting class components to functional components

--- server/models/patient-model.js
+++ server/models/patient-model.js
@@ -1,13 +1,10 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Patient = new Schema(
-    {
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        dob: { type: String, required: true },
-    },
-   
-)
+const Patient = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  dob: { type: String, required: true },
+});
 
-module.exports = mongoose.model('patients', Patient)
+module.exports = mongoose.model("patients", Patient);
